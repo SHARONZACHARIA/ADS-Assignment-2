916b42e3a87e136b9beaba6788c4a98e262e3079
Sharon Zacharia <zachariasharon@gmail.com>
2023-12-04 00:15:41 +0000

    applied stats

--- stats_trends.py
+++ stats_trends.py
@@ -3,33 +3,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-dataUri = 'Gender_Stats_Data.csv'
+dataUri = 'DataSet/Gender_StatsData.csv'
 
 def readDataset(dataUrl):
     dataset = pd.read_csv(dataUrl)
-    # country_transposed_data = dataset.set_index('Country Name')
-    # year_transposed_data = dataset.transpose()
-    # print(dataset.describe())
-    # print(dataset.head(25))
     return  dataset
 
 
 genderdata = readDataset(dataUri)
+# print(genderdata.describe())
+print(genderdata.corr())
 
 """  Applying Statistical Methods on  Expected years of schooling female"""
 
 year_of_schooling = genderdata.loc[genderdata['Indicator Code'] == 'SE.SCH.LIFE.FE']
 
+describe_gender_female = year_of_schooling ["1990"].describe()
+print ('skewness',describe_gender_female)
 
-describe_gender = year_of_schooling ["1975"].describe()
-print ('skewness',describe_gender)
+skew_gender_female = year_of_schooling ["1990"].skew()
+print ('skewness',skew_gender_female)
 
-skew_gender = year_of_schooling ["1975"].skew()
-print ('skewness',skew_gender)
-
-stdev_gender = year_of_schooling ["1975"].std()
-print ('standard deviation',stdev_gender)
+stdev_gender_female = year_of_schooling ["1990"].std()
+print ('standard deviation',stdev_gender_female)
 
 
 """  Applying Statistical Methods on  Expected years of schooling male """
 
+year_of_schooling = genderdata.loc[genderdata['Indicator Code'] == 'SE.SCH.LIFE.MA']
+
+describe_gender_male = year_of_schooling ["1990"].describe()
+print ('skewness',describe_gender_male)
+
+skew_gender_male = year_of_schooling ["1990"].skew()
+print ('skewness',skew_gender_male)
+
+stdev_gender_male = year_of_schooling ["1990"].std()
+print ('standard deviation',stdev_gender_male)
