82d84eff3ac5ec10a180bbc750bfcdb3cfe6c921
Sharon Zacharia <zachariasharon@gmail.com>
2023-12-06 20:46:05 +0000

    Data cleaning added

--- stats_trends.py
+++ stats_trends.py
@@ -5,42 +5,75 @@ import matplotlib.pyplot as plt
 
 dataUri = 'DataSet/Gender_StatsData.csv'
 
-def readDataset(dataUrl):
+def getDataset(dataUrl):
     dataset = pd.read_csv(dataUrl)
     return  dataset
 
 
-genderdata = readDataset(dataUri)
-print(genderdata.corr())
+"""Defining Feature codes and country named to analyse """
 
-"""   Statistical Analysis on Expected years of schooling female """
+INDICATOR_CODES=['SP.POP.TOTL', 'SP.POP.TOTL.FE.IN', 'SP.POP.TOTL.MA.IN',
+ 'SP.DYN.CBRT.IN', 'SP.DYN.CDRT.IN',
+ 'SE.COM.DURS',
+ 'SL.IND.EMPL.ZS', 'SL.AGR.EMPL.ZS', 'SL.AGR.EMPL.FE.ZS', 'SL.IND.EMPL.FE.ZS', 'SL.UEM.TOTL.ZS',
+ 'NY.GDP.MKTP.CD',
+ 'NY.ADJ.NNTY.PC.KD.ZG', 'NY.GSR.NFCY.CD', 'NV.AGR.TOTL.CD',
+ 'EG.USE.ELEC.KH.PC', 'EG.FEC.RNEW.ZS', 'EG.USE.COMM.FO.ZS']
 
-year_of_schooling = genderdata.loc[genderdata['Indicator Code'] == 'SE.SCH.LIFE.FE']
+countryMap={
+    "US": "USA",
+    "IN":"India",
+    "CN": "China",
+    "JP": "Japan",
+    "CA": "Canada",
+    "GB": "Great Britain",
+    "ZA": "South Africa"
+}
 
-describe_gender_female = year_of_schooling ["1990"].describe()
-print ('skewness',describe_gender_female)
-
-skew_gender_female = year_of_schooling ["1990"].skew()
-print ('skewness',skew_gender_female)
-
-stdev_gender_female = year_of_schooling ["1990"].std()
-print ('standard deviation',stdev_gender_female)
-
-
-""" Statistical Analysis  on  Expected years of schooling male """
-
-year_of_schooling = genderdata.loc[genderdata['Indicator Code'] == 'SE.SCH.LIFE.MA']
-
-describe_gender_male = year_of_schooling ["1990"].describe()
-print ('skewness',describe_gender_male)
-
-skew_gender_male = year_of_schooling ["1990"].skew()
-print ('skewness',skew_gender_male)
-
-stdev_gender_male = year_of_schooling ["1990"].std()
-print ('standard deviation',stdev_gender_male)
+def remove_missing_features(df):
+    
+    if df is None:
+        print("No DataFrame received!")
+        return
+    
+    updated_df=df.copy()
+    print("Removed missing features for: " + updated_df.iloc[0]['Country'])
+    no_missing_vals=df.isnull().sum()
+    n_missing_index_list = list(no_missing_vals.index)
+    missing_percentage = no_missing_vals[no_missing_vals!=0]/df.shape[0]*100
+    cols_to_trim=[]
+    
+    
+    for i,val in enumerate(missing_percentage):
+        if val > 75:
+            cols_to_trim.append(n_missing_index_list[i])
 
 
-""" Data analysis using plots """
+    if len(cols_to_trim) > 0:
+        updated_df=updated_df.drop(columns=cols_to_trim)
+        print("Dropped Columns:" + str(cols_to_trim))
+    else:
+        print("Nothing  dropped")
+    return updated_df
 
-print(genderdata.shape)
+
+def fill_missing_values(df):
+    
+    if df is None:
+        print("No DataFrame received")
+        return
+
+    df_cp=df.copy()
+    
+    print("Filling missing features for: " + df_cp.iloc[0]['Country'])
+    
+    cols_list=list(df_cp.columns)
+    cols_list.pop()
+
+    df_cp.fillna(value=pd.np.nan, inplace=True)
+    
+    for col in cols_list:
+        df_cp[col].fillna((df_cp[col].mean()), inplace=True)
+
+    print("Filling missing values completed")
+    return df_cp
