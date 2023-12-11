c893c5c850c3f00d4bee5d4aa8ecf56418d771a1
Sharon Zacharia <zachariasharon@gmail.com>
2023-12-11 20:23:38 +0000

    commit

--- stats_trends.py
+++ stats_trends.py
@@ -8,21 +8,17 @@ dataUri = 'DataSet/Gender_StatsData.csv'
 pivoted_data_uri = 'DataSet/PivotedDataset.csv'
 cleaned_data_uri = 'DataSet/CleanedDataset.csv'
 
-
 def getDataset(dataUrl):
     dataset = pd.read_csv(dataUrl)
     return dataset
-
 
 """ Defining Feature codes and country names to analyse """
 
 COLUMN_CODES = ['Country Name', 'Country Code', 'Year', 'SP.POP.TOTL', 'SP.POP.TOTL.FE.IN', 'SP.POP.TOTL.MA.IN',
                 'SP.DYN.CBRT.IN', 'SP.DYN.CDRT.IN',
-                'SE.COM.DURS',
                 'SL.IND.EMPL.ZS', 'SL.AGR.EMPL.ZS', 'SL.AGR.EMPL.FE.ZS', 'SL.IND.EMPL.FE.ZS', 'SL.UEM.TOTL.ZS',
                 'NY.GDP.MKTP.CD',
-                'NY.ADJ.NNTY.PC.KD.ZG']
-
+                'NY.ADJ.NNTY.PC.KD.ZG','SE.ADT.1524.LT.FE.ZS','SE.ADT.1524.LT.MA.ZS']
 
 featureMap = {
     "SP.POP.TOTL": "Total Population",
@@ -30,16 +26,16 @@ featureMap = {
     "SP.POP.TOTL.MA.IN": "Male Population",
     "SP.DYN.CBRT.IN": "Birth Rate",
     "SP.DYN.CDRT.IN": "Death Rate",
-    "SE.COM.DURS": "Compulsory Education Dur.",
     "SL.IND.EMPL.ZS": "Employment in Industry(%)",
     "SL.AGR.EMPL.ZS": "Employment in Agriculture(%)",
     "SL.AGR.EMPL.FE.ZS": "Female Employment in Agriculture(%)",
     "SL.IND.EMPL.FE.ZS": "Female Employment in Industry(%)",
     "SL.UEM.TOTL.ZS": "Unemployment(%)",
     "NY.GDP.MKTP.CD": "GDP in USD",
-    "NY.ADJ.NNTY.PC.KD.ZG": "National Income per Capita"
+    "NY.ADJ.NNTY.PC.KD.ZG": "National Income per Capita",
+    "SE.ADT.1524.LT.MA.ZS" : "Literacy rate % male (ages 15-24)",
+    "SE.ADT.1524.LT.FE.ZS" : "Literacy rate % 'female (ages 15-24)"
 }
-
 
 countryMap = {
     "US": "USA",
@@ -63,7 +59,6 @@ pivoted_df = melted_df.pivot_table(
 
 filtered_columns = [col for col in pivoted_df.columns if col in COLUMN_CODES]
 df_filtered = pivoted_df[filtered_columns]
-
 
 # cleaning the transformed data set
 # Fill missing values with the mean of the column
@@ -107,17 +102,25 @@ heatmap_data = sns.heatmap(correaltion_matrix_ZAF , annot=True,fmt=".1g", vmax=1
 plt.title('Correlation Matrix for South Africa')
 plt.show()
 
-# bar graphs 
-# Filtering for India and China for the years 1960 and 2022
-df_population_by_year = pd.read_csv(cleaned_data_uri)
+# # bar graphs 
+# # Filtering for India and China for the years 1960 and 2022
+cleaned_df = pd.read_csv(cleaned_data_uri)
 
-filtered_population = df_population_by_year[
-    ((df_population_by_year['Country Name'] == 'India') | (df_population_by_year['Country Name'] == 'China') | 
-    (df_population_by_year['Country Name'] == 'United Kingdom') | (df_population_by_year['Country Name'] == 'South Africa')) &
-    ((df_population_by_year['Year'] == 1960) | (df_population_by_year['Year'] == 1990) | (df_population_by_year['Year'] == 2022))]
+filtered_population = cleaned_df[
+    ((cleaned_df['Country Name'] == 'India') | (cleaned_df['Country Name'] == 'China') | 
+    (cleaned_df['Country Name'] == 'United Kingdom') | (cleaned_df['Country Name'] == 'South Africa')) &
+    ((cleaned_df['Year'] == 1960) | (cleaned_df['Year'] == 1990) | (cleaned_df['Year'] == 2022))]
 
 filtered_population = filtered_population[["Country Name","Year","SP.POP.TOTL"]]
- pivoted_population_df = filtered_population.pivot(index='Country Name', columns='Year', values='SP.POP.TOTL').reset_index()
+pivoted_population_df = filtered_population.pivot(index='Country Name', columns='Year', values='SP.POP.TOTL').reset_index()
 pivoted_population_df.plot(kind='bar',x='Country Name',y=[1960, 1990, 2022])
 plt.xticks(rotation=30, horizontalalignment="center")
+plt.show()
+
+
+#line graphs 
+literacy_FE_India = cleaned_df[cleaned_df['Country Name']=="India"]
+literacy_FE_China = cleaned_df[cleaned_df['Country Name']=="China"]
+plt.plot(literacy_FE_China["Year"],literacy_FE_China["SE.ADT.1524.LT.FE.ZS"])
+plt.plot(literacy_FE_India["Year"],literacy_FE_India["SE.ADT.1524.LT.FE.ZS"])
 plt.show()
