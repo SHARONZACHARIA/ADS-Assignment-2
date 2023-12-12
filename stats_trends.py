3691673f11b2a65e6eb880204fa89a6910788bce
Sharon Zacharia <zachariasharon@gmail.com>
2023-12-12 10:41:52 +0000

    added report page

--- stats_trends.py
+++ stats_trends.py
@@ -120,28 +120,21 @@ population_Gender_India = cleaned_df[cleaned_df['Country Name']=="India"] # Choo
 population_Gender_China = cleaned_df[cleaned_df['Country Name']=="China"]
 population_Gender_Usa = cleaned_df[cleaned_df['Country Name']=="United States"] # Choose the column for the y-axis
 population_Gender_Sa = cleaned_df[cleaned_df['Country Name']=="South Africa"]
-# Plotting
-# Plotting
 
 
 fig, axs = plt.subplots(3, 1, figsize=(8, 18))
-
- 
 axs[0].plot(population_Gender_China["Year"], population_Gender_China["SP.POP.TOTL.FE.IN"], linestyle='-', color='g')
 axs[0].plot(population_Gender_China["Year"], population_Gender_China["SP.POP.TOTL.MA.IN"], linestyle='-', color='b')
-
 
 axs[1].set_xlabel("Year")
 axs[1].set_ylabel("Gender") # Adjust figure size if needed
 axs[1].plot(population_Gender_Usa["Year"], population_Gender_Usa["SP.POP.TOTL.FE.IN"], linestyle='-', color='g')
 axs[1].plot(population_Gender_Usa["Year"], population_Gender_Usa["SP.POP.TOTL.MA.IN"], linestyle='-', color='b')
 
-
 axs[2].set_xlabel("Year")
 axs[2].set_ylabel("Gender") # Adjust figure size if needed
 axs[2].plot(population_Gender_Sa["Year"], population_Gender_Sa["SP.POP.TOTL.FE.IN"], linestyle='-', color='g')
 axs[2].plot(population_Gender_Sa["Year"], population_Gender_Sa["SP.POP.TOTL.MA.IN"], linestyle='-', color='b')
-
 plt.show()
 
 unemployment_India = cleaned_df[(cleaned_df['Country Name']=="India") & (cleaned_df['Year'] >= cleaned_df['Year'].max() - 9)]# Choose the column for the y-axis
